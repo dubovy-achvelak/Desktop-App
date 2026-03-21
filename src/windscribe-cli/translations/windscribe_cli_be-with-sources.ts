8de91eb65c3fb1882cbe37bd3bf3c43ba96a2095
Dubovy Achvełak <268134219+dubovy-achvelak@users.noreply.github.com>
2026-03-21 21:35:38 +0300

    Add translation with sources of cli app to be

---
+++ src/windscribe-cli/translations/windscribe_cli_be-with-sources.ts
@@ -0,0 +1,435 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="be_BY">
+<context>
+    <name>BackendCommander</name>
+    <message>
+        <location filename="../backendcommander.cpp" line="350"/>
+        <source>Data usage: %1 / %2</source>
+        <translation>Выкарыстаньне трафіку: %1 / %2</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="352"/>
+        <source>Unlimited</source>
+        <translation>Безьліміт</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="70"/>
+        <source>No locations.</source>
+        <translation>Няма месцазнаходжаньняў.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="447"/>
+        <source>Could not rotate IP.  Please check that you have Windscribe Pro or have this location in your plan, or try again later.</source>
+        <translation>Не ўдалося зьмяніць IP-адрас.  Праверце, ці маеце Windscribe Pro, альбо ці ўваходзіць гэтае месцазнаходжаньне ў вашы тарыфны плян, альбо паўтарыце спробу пазьней.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="454"/>
+        <source>Could not favourite IP.  Please check that you have Windscribe Pro or have this location in your plan, or try again later.</source>
+        <translation>Не ўдалося дадаць IP-адрас у абранае.  Праверце, ці маеце Windscribe Pro, альбо ці ўваходзіць гэтае месцазнаходжаньне ў вашы тарыфны плян, альбо паўтарыце спробу пазьней.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="461"/>
+        <source>Could not unfavourite IP.  Please check that the provided IP is valid.</source>
+        <translation>Не ўдалося прыбраць IP-адрас з абранага.  Праверце, ці правільны ўведзены IP-адрас.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="445"/>
+        <source>IP rotate already in progress.</source>
+        <translation>Зьмена IP-адраса ўжо ідзе.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="77"/>
+        <source>No AmneziaWG configurations available.</source>
+        <translation>Няма даступных канфіґурацый AmneziaWG.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../backendcommander.cpp" line="104"/>
+        <source>Aborting: app did not start in time</source>
+        <translation>Перапыненьне: праґрама не запусцілася ў адведзены час</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="113"/>
+        <source>Aborting: IPC communication error</source>
+        <translation>Перапыненьне: памылка межпрацэснай сувязі (IPC)</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="132"/>
+        <location filename="../backendcommander.cpp" line="148"/>
+        <location filename="../backendcommander.cpp" line="187"/>
+        <location filename="../backendcommander.cpp" line="265"/>
+        <location filename="../backendcommander.cpp" line="281"/>
+        <location filename="../backendcommander.cpp" line="293"/>
+        <location filename="../backendcommander.cpp" line="305"/>
+        <source>Not logged in</source>
+        <translation>Уваход ва ўліковы запіс не выкананы</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="159"/>
+        <source>Firewall already on</source>
+        <translation>Брандмаўэр ужо ўключаны</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="168"/>
+        <source>Firewall already off</source>
+        <translation>Брандмаўэр ужо выключаны</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="172"/>
+        <source>Firewall set to always on and can&apos;t be turned off</source>
+        <translation>Брандмаўэр заўсёды уключаны і не можа быць выключаны</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="213"/>
+        <source>Already logged in</source>
+        <translation>Ужо ўвайшлі ва ўліковы запіс</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="240"/>
+        <source>Already logged out</source>
+        <translation>Ужо выйшлі з уліковага запісу</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="269"/>
+        <location filename="../strings.cpp" line="137"/>
+        <source>No update available</source>
+        <translation>Абнаўленьняў няма</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="7"/>
+        <source>Internet connectivity: %1</source>
+        <translation>Падлучэньне да Інтэрнэту: %1</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="12"/>
+        <source>Login state: %1</source>
+        <translation>Стан уваходу: %1</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="16"/>
+        <source>Logged out</source>
+        <translation>Выйшлі</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="18"/>
+        <source>Logging in</source>
+        <translation>Уваход ідзе</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="20"/>
+        <source>Logged in</source>
+        <translation>Увайшлі</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="23"/>
+        <location filename="../strings.cpp" line="99"/>
+        <source>Error: %1</source>
+        <translation>Памылка: %1</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="27"/>
+        <source>No internet connectivity</source>
+        <translation>Адсутнічае падлучэньне да Інтэрнэту</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="29"/>
+        <source>No API connectivity</source>
+        <translation>Адсутнічае падлучэньне да API</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="31"/>
+        <source>Incorrect username, password, or 2FA code</source>
+        <translation>Няправільныя імя карыстальніка, пароль альбо код 2FA</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="38"/>
+        <source>SSL error</source>
+        <translation>Памылка SSL</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="40"/>
+        <source>Session expired</source>
+        <translation>Сесія скончаная</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="42"/>
+        <source>Rate limited</source>
+        <translation>Доступ абмежаваны</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="48"/>
+        <source>Unknown error</source>
+        <translation>Невядомая памылка</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="55"/>
+        <source>Connect state: %1</source>
+        <translation>Стан злучэньня: %1</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="61"/>
+        <source>Connected: %1</source>
+        <translation>Падлучана: %1</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="63"/>
+        <source>Connected</source>
+        <translation>Падлучана</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="68"/>
+        <source>[Network interference]</source>
+        <translation>[Перашкоды ў сетцы]</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="73"/>
+        <source>Connecting: %1</source>
+        <translation>Ідзе падлучэньне: %1</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="75"/>
+        <source>Connecting</source>
+        <translation>Ідзе падлучэньне</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="78"/>
+        <source>Disconnecting</source>
+        <translation>Ідзе адлучэньне</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="86"/>
+        <source>Disconnected</source>
+        <translation>Адлучана</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="89"/>
+        <source>Error: Location does not exist or is disabled</source>
+        <translation>Памылка: месцазнаходжаньне не існуе альбо адключана</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="91"/>
+        <source>Error: You are out of data, or your account has been disabled. Upgrade to Pro to continue using Windscribe</source>
+        <translation>Памылка: у вас скончыўся трафік альбо ўліковы запіс заблякаваны. Перайдзіце на Pro, каб працягваць карыстацца Windscribe</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="93"/>
+        <source>Error: Unable to start custom DNS service</source>
+        <translation>Памылка: не ўдалося запусьціць уласную службу DNS</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="95"/>
+        <source>Error: WireGuard adapter setup failed</source>
+        <translation>Памылка: не ўдалося наладзіць адаптар WireGuard</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="97"/>
+        <source>Error: Could not retrieve WireGuard configuration</source>
+        <translation>Памылка: не ўдалося атрымаць канфіґурацыю WireGuard</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="102"/>
+        <source>Unknown state</source>
+        <translation>Невядомы стан</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="108"/>
+        <source>Protocol: %1:%2</source>
+        <translation>Пратакол: %1:%2</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="113"/>
+        <source>Firewall state: %1</source>
+        <translation>Стан брандмаўэра: %1</translation>
+    </message>
+    <message>
+        <location filename="../../client/client-common/types/enums.cpp" line="181"/>
+        <location filename="../strings.cpp" line="116"/>
+        <source>Always On</source>
+        <translation>Заўсёды ўключаны</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="119"/>
+        <source>On</source>
+        <translation>Уключаны</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="122"/>
+        <source>Off</source>
+        <translation>Выключаны</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="143"/>
+        <source>Update error: download failed. Please try again or try a different network.</source>
+        <translation>Памылка абнаўленьня: не ўдалося спампаваць. Паўтарыце спробу, альбо падлучыцеся да іншай сеткі.</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="145"/>
+        <source>Update error: %1</source>
+        <translation>Памылка абнаўленьня: %1</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="135"/>
+        <source>Update available: %1</source>
+        <translation>Даступнае абнаўленьне: %1</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="7"/>
+        <source>available</source>
+        <translation>даступнае</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="7"/>
+        <source>unavailable</source>
+        <translation>недаступнае</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="81"/>
+        <source>Disconnected due to reaching WireGuard key limit.  Use &quot;windscribe-cli keylimit delete&quot; if you want to delete the oldest key instead, and try again.</source>
+        <translation>Адлучана: дасягнуты ліміт ключоў WireGuard.  Выкарыстоўвайце &quot;windscribe-cli keylimit delete&quot;, каб выдаліць найстарэйшы ключ, і паспрабуйце яшчэ раз.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="379"/>
+        <source>Downloading: %1%</source>
+        <translation>Ідзе спампоўваньне: %1%</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="219"/>
+        <source>Username too short</source>
+        <translation>Імя карыстальніка занадта кароткае</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="224"/>
+        <source>Password too short</source>
+        <translation>Пароль занадта кароткі</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="423"/>
+        <source>Connection is in progress.  Use &apos;windscribe-cli status&apos; to check for connection status.</source>
+        <translation>Ідзе падлучэньне.  Выкарыстоўвайце &apos;windscribe-cli status&apos;, каб праверыць стан злучэньня.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="426"/>
+        <source>Disconnection is in progress.  Use &apos;windscribe-cli status&apos; to check for connection status.</source>
+        <translation>Ідзе адлучэньне.  Выкарыстоўвайце &apos;windscribe-cli status&apos;, каб праверыць стан злучэньня.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="429"/>
+        <source>Firewall is on.</source>
+        <translation>Брандмаўэр уключаны.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="432"/>
+        <source>Firewall is off.</source>
+        <translation>Брандмаўэр выключаны.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="438"/>
+        <source>Logs sent.</source>
+        <translation>Журналы адасланыя.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="441"/>
+        <source>Preferences reloaded.</source>
+        <translation>Налады перазагружаныя.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="435"/>
+        <source>Key limit behaviour is set.</source>
+        <translation>Паводзіны пры дасягненьні ліміту ключоў зададзеныя.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="496"/>
+        <source>Connection has been overridden by another command.</source>
+        <translation>Падлучэньне было перавызначана іншай камандай.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="516"/>
+        <source>Disconnection has been overridden by another command.</source>
+        <translation>Адлучэньне было перавызначана іншай камандай.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="152"/>
+        <source>Already disconnected</source>
+        <translation>Ужо адключана</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="554"/>
+        <source>Logging out</source>
+        <translation>Выхад ідзе</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="33"/>
+        <source>Need 2FA code</source>
+        <translation>Патрэбны код 2FA</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="44"/>
+        <source>Incorrect 2FA code</source>
+        <translation>Некарэктны код 2FA</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="153"/>
+        <source>(Device name: %1)</source>
+        <translation>(Імя прылады: %1)</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="126"/>
+        <location filename="../backendcommander.cpp" line="208"/>
+        <location filename="../backendcommander.cpp" line="235"/>
+        <location filename="../backendcommander.cpp" line="250"/>
+        <location filename="../backendcommander.cpp" line="260"/>
+        <source>Internet connectivity is not available.  Try again later.</source>
+        <translation>Падлучэньне да Інтэрнэту недаступнае.  Паўтарыце спробу пазьней.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="535"/>
+        <source>Please type the numbers above to continue:</source>
+        <translation>Каб працягнуць, увядзіце лічбы вышэй:</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="184"/>
+        <source> (10 Gbps)</source>
+        <translation> (10 Гбіт/с)</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="180"/>
+        <source> (Disabled)</source>
+        <translatorcomment>Месцазнаходжаньне</translatorcomment>
+        <translation> (Адключанае)</translation>
+    </message>
+    <message>
+        <location filename="../strings.cpp" line="178"/>
+        <source> (Pro)</source>
+        <translation> (Pro)</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="285"/>
+        <location filename="../backendcommander.cpp" line="297"/>
+        <source>Not connected</source>
+        <translation>Не падлучана</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="449"/>
+        <source>IP rotated.</source>
+        <translation>IP-адрас зьменены.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="456"/>
+        <source>IP favorited.</source>
+        <translation>IP-адрас дададзены ў абранае.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="463"/>
+        <source>IP unfavorited.</source>
+        <translation>IP-адрас прыбраны з абранага.</translation>
+    </message>
+    <message>
+        <location filename="../backendcommander.cpp" line="310"/>
+        <source>Invalid IP address</source>
+        <translation>Некарэктны IP-адрас</translation>
+    </message>
+</context>
+</TS>
