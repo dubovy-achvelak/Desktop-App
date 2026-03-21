95443dd82d666a7e7c405aa3167462836fca5734
Dubovy Achvełak <268134219+dubovy-achvelak@users.noreply.github.com>
2026-03-21 21:48:00 +0300

    Add translation with sources of installer to be

---
+++ src/installer/common/translations/windscribe_installer_be-with-sources.ts
@@ -0,0 +1,303 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="be_BY">
+<context>
+    <name>AlertWindow</name>
+    <message>
+        <location filename="../alertwindow.cpp" line="27"/>
+        <source>ESC</source>
+        <translation>ESC</translation>
+    </message>
+</context>
+<context>
+    <name>InitialWindow</name>
+    <message>
+        <location filename="../initialwindow.cpp" line="36"/>
+        <source>Read EULA</source>
+        <translation>Чытаць Карыстальніцкае пагадненьне</translation>
+    </message>
+</context>
+<context>
+    <name>InstallButton</name>
+    <message>
+        <location filename="../installbutton.cpp" line="12"/>
+        <source>Install</source>
+        <translation>Усталяваць</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="455"/>
+        <source>Invalid path</source>
+        <translation>Няправільны шлях</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="120"/>
+        <location filename="../mainwindow.cpp" line="129"/>
+        <location filename="../mainwindow.cpp" line="256"/>
+        <source>Installation failed</source>
+        <translation>Усталяваньне не ўдалося</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="247"/>
+        <source>The installation could not be completed successfully. Please contact our Technical Support.</source>
+        <translation>Не ўдалося завяршыць усталяваньне. Зьвярніцеся ў службу тэхнічнай падтрымкі.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="303"/>
+        <source>Select a folder in the list below and click OK.</source>
+        <translatorcomment>У Windows перакладзе OK -&gt; ОК</translatorcomment>
+        <translation>Абярыце каталёґ з сьпіса ніжэй і націсьніце ОК.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="456"/>
+        <source>The specified installation path is not on the system drive. To ensure the security of the application, and your system, it must be installed on the same drive as Windows. The installation folder has been reset to the default.</source>
+        <translation>Абраны шлях усталяваньня знаходзіцца не на сыстэмным дыску. Каб забясьпечыць бясьпеку праґрамы і вашай сыстэмы, яе трэба ўсталяваць на той жа дыск, што і Windows. Каталёґ ўсталяваньня быў вернуты да прадвызначанага.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="325"/>
+        <location filename="../mainwindow.cpp" line="382"/>
+        <source>OK</source>
+        <translation>Добра</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="231"/>
+        <source>The installation was cancelled. Administrator privileges are required to install the application.</source>
+        <translation>Усталяваньне было адменена. Для ўсталяваньня праґрамы патрэбныя правы адміністратара.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="233"/>
+        <source>Windscribe is running and could not be closed. Please close the application manually and try again.</source>
+        <translation>Windscribe працуе, і яго не ўдалося закрыць. Закрыйце праґраму ўручную і паспрабуйце зноў.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="347"/>
+        <source>Quit Windscribe Installer?</source>
+        <translation>Выйсьці з усталёўніка Windscribe?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="235"/>
+        <source>The installer could not connect to the privileged helper tool. Please try again.</source>
+        <translation>Усталёўніку не ўдалося падлучыцца да дапаможнай службы з правамі адміністратара. Паспрабуйце яшчэ раз.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="237"/>
+        <source>An existing installation of Windscribe could not be removed. Please uninstall the application manually and try again.</source>
+        <translation>Не ўдалося выдаліць існую ўсталёўку Windscribe. Выдаліце праґраму ўручную і паспрабуйце зноў.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="350"/>
+        <source>Quit</source>
+        <translation>Зачыніць</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="353"/>
+        <location filename="../mainwindow.cpp" line="385"/>
+        <source>Cancel</source>
+        <translation>Скасаваць</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="130"/>
+        <source>You don&apos;t have sufficient permissions to run this application. Administrative privileges are required to install Windscribe.</source>
+        <translation>У вас недастаткова правоў, каб запусьціць гэтую праґраму. Для ўсталёўваньня Windscribe патрэбныя правы адміністратара.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="239"/>
+        <source>The uninstaller for the existing installation of Windscribe could not be found. Please uninstall the application manually and try again.</source>
+        <translation>Не ўдалося знайсьці праґраму для выдаленьня існай усталёўкі Windscribe. Выдаліце праґраму ўручную і праспрабуйце зноў.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="374"/>
+        <source>Security Warning</source>
+        <translation>Папярэджаньне бясьпекі</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="377"/>
+        <source>Installation to a custom folder may allow an attacker to tamper with the Windscribe application. To ensure the security of the application, and your system, we strongly recommend you install to the default location in the &apos;Program Files&apos; folder. Click OK to continue with the custom folder or Cancel to use the default location.</source>
+        <translation>Усталёўваньне ва ўласны каталёґ можа дазволіць зламысьніку зьмяніць файлы праґрамы Windscribe. Каб забясьпечыць бясьпеку праґрамы і вашай сыстэмы, мы настойліва раім усталёўваць яе ў прадвызначаны каталёґ &quot;Program Files&quot;. Націсьніце &quot;Добра&quot;, каб працягнуць з уласным каталёґам, альбо &quot;Скасаваць&quot;, каб выкарыстоўваць прадвызначаны каталёґ.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="121"/>
+        <source>Your current username is &apos;windscribe&apos;, which is needed by the Windscribe app. Windscribe can&apos;t be installed.</source>
+        <translation>Ваша бягучае імя карыстальніка — &quot;windscribe&quot;, яно патрэбнае праграме Windscribe, таму ўсталёўка немагчымая.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="241"/>
+        <source>Installation to your custom folder was unsuccessful. Please uninstall the application manually, ensure no applications are accessing the custom folder, and try again.</source>
+        <translation>Усталёўваньне ва ўласны каталёґ не ўдалося. Выдаліце праґраму ўручную, пераканайцеся, што ніякія праґрамы не маюць доступу да гэтага каталёґа, і паспрабуйце яшчэ раз.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="245"/>
+        <source>The custom installation folder could not be deleted. Please uninstall the application manually, ensure no applications are accessing the custom folder, and try again.</source>
+        <translation>Не ўдалося выдаліць уласны каталёґ ўсталёўваньня. Выдаліце праґраму ўручную, пераканайцеся, што ніякія праґрамы не маюць доступу да гэтага каталёґа, і паспрабуйце яшчэ раз.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="243"/>
+        <source>The custom installation folder is not empty. As a security precaution, Windscribe can only be installed to an empty folder. Please delete all files from the folder and try again.</source>
+        <translation>Уласны каталёґ усталёўваньня не пусты. У якасьці меры бясьпекі Windscribe можна ўсталяваць толькі ў пусты каталёґ. Выдаліце ўсе файлы з каталёґа і паспрабуйце яшчэ раз.</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressDisplay</name>
+    <message>
+        <location filename="../progressdisplay.cpp" line="32"/>
+        <source>%1%</source>
+        <translation>%1%</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="242"/>
+        <source>Windscribe Installer</source>
+        <translation>Усталёўнік Windscribe</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="243"/>
+        <source>This version of the Windscribe app will not operate correctly on your PC.  Please download the &apos;ARM64&apos; version from the Windscribe website to ensure optimal compatibility and performance.</source>
+        <translation>Гэта вэрсія праґрамы Windscribe не будзе належным чынам працаваць на вашым ПК.  Спампуйце вэрсію ARM64 з вэб-сайту Windscribe, каб забясьпечыць сумяшчальнасьць і хуткасьць працы.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="252"/>
+        <source>Windscribe Install Options</source>
+        <translation>Парамэтры ўсталёўкі Windscribe</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="254"/>
+        <source>The Windscribe installer accepts the following optional commmand-line parameters: </source>
+        <translation>Усталёўнік Windscribe прымае наступныя неабавязковыя парамэтры каманднага радка: </translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="255"/>
+        <source>Show this information.</source>
+        <translation>Паказаць інфармацыю.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="256"/>
+        <source>Do not launch the application after installation.</source>
+        <translation>Не запускаць праґраму пасьля ўсталёўваньня.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="258"/>
+        <source>Delete existing preferences, logs, and other data, if they exist.</source>
+        <translation>Выдаліць існыя налады, журналы і іншыя дадзеныя, калі яны ёсьць.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="259"/>
+        <source>Overrides the default installation directory. Installation directory must be on the system drive.</source>
+        <translation>Перавызначае прадвызначаны каталёґ усталёўкі. Ён павінен знаходзіцца на сыстэмным дыску.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="260"/>
+        <source>Sets the username the application will use to automatically log in when first launched.</source>
+        <translation>Задае імя карыстальніка, якое праґрама будзе выкарыстоўваць для аўтаматычнага ўваходу пры першым запуску.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="261"/>
+        <source>Sets the password the application will use to automatically log in when first launched.</source>
+        <translation>Задае пароль, які праґрама будзе выкарыстоўваць для аўтаматычнага ўваходу пры першым запуску.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="299"/>
+        <location filename="../../windows/installer/main.cpp" line="314"/>
+        <location filename="../../windows/installer/main.cpp" line="328"/>
+        <location filename="../../windows/installer/main.cpp" line="340"/>
+        <location filename="../../windows/installer/main.cpp" line="348"/>
+        <location filename="../../windows/installer/main.cpp" line="355"/>
+        <location filename="../../windows/installer/main.cpp" line="363"/>
+        <location filename="../../windows/installer/main.cpp" line="370"/>
+        <location filename="../../windows/installer/main.cpp" line="384"/>
+        <location filename="../../windows/installer/main.cpp" line="392"/>
+        <source>Windscribe Install Error</source>
+        <translation>Памылка ўсталёўвання Windscribe</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="300"/>
+        <source>The -dir parameter was specified but the directory path was not.</source>
+        <translation>Парамэтар -dir быў пазначаны, але шлях да каталёґу не зададзены.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="315"/>
+        <source>The -username parameter was specified but the username was not.</source>
+        <translation>Парамэтар -username быў пазначаны, але імя карыстальніка не зададзена.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="329"/>
+        <source>The -password parameter was specified but the password was not.</source>
+        <translation>Парамэтар -password быў пазначаны, але пароль не зададзены.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="342"/>
+        <source>Incorrect number of arguments passed to installer.</source>
+        <translation>Усталёўніку перададзена няправільная колькосьць арґумэнтаў.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="343"/>
+        <location filename="../../windows/installer/main.cpp" line="358"/>
+        <location filename="../../windows/installer/main.cpp" line="373"/>
+        <source>Use the -help argument to see available arguments and their format.</source>
+        <translation>Выкарыстоўвайце парамэтар -help, каб паглядзець даступныя парамэтры і іх фармат.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="349"/>
+        <source>The specified installation path is not on the system drive.  To ensure the security of the application, and your system, it must be installed on the same drive as Windows.</source>
+        <translation>Пазначаны шлях усталёўкі не разьмешчаны на сыстэмным дыску.  Каб забясьпечыць бясьпеку праґрамы і вашай сыстэмы, яе неабходна ўсталяваць на той жа дыск, што і Windows.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="357"/>
+        <source>A username was specified but its corresponding password was not provided.</source>
+        <translation>Імя карыстальніка было пазначана, але адпаведны пароль не ўказаны.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="364"/>
+        <source>Your username should not be an email address. Please try again.</source>
+        <translation>Імя карыстальніка не павінна быць адрасам электроннай пошты. Паспрабуйце яшчэ раз.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="372"/>
+        <source>A password was specified but its corresponding username was not provided.</source>
+        <translation>Пароль быў пазначаны, але адпаведнае імя карыстальніка не ўказана.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="385"/>
+        <source>The installer was unable to determine if it is running with administrator rights.  Please report this failure to Windscribe support.</source>
+        <translation>Усталёўнік не здолеў вызначыць, ці запушчаны ён з правамі адміністратара.  Паведаміце пра гэтую памылку ў службу падтрымкі Windscribe.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="257"/>
+        <source>Instructs the installer to hide its user interface.</source>
+        <translation>Загадвае ўсталёўніку схаваць карыстальніцкі інтэрфэйс.</translation>
+    </message>
+    <message>
+        <location filename="../../windows/installer/main.cpp" line="393"/>
+        <source>You don&apos;t have sufficient permissions to run this application. Administrative privileges are required to install Windscribe.</source>
+        <translation>У вас не дастаткова правоў для запуску гэтай праґрамы. Для ўсталёўкі Windscribe неабходныя правы адміністратара.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <location filename="../settingswindow.cpp" line="31"/>
+        <location filename="../settingswindow.cpp" line="33"/>
+        <source>Install Settings</source>
+        <translation>Налады ўсталяваньня</translation>
+    </message>
+    <message>
+        <location filename="../settingswindow.cpp" line="36"/>
+        <source>OK</source>
+        <translation>Добра</translation>
+    </message>
+    <message>
+        <location filename="../settingswindow.cpp" line="44"/>
+        <location filename="../settingswindow.cpp" line="63"/>
+        <source>Factory Reset</source>
+        <translation>Ськінуць налады</translation>
+    </message>
+    <message>
+        <location filename="../settingswindow.cpp" line="57"/>
+        <source>Create shortcut</source>
+        <translation>Стварыць цэтлік</translation>
+    </message>
+</context>
+</TS>
